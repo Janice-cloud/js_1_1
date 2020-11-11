454f4b6b238db6f0cc22fc31606b90349ad06eaf
Janice Leung <janice.leung2008@gmail.com>
2020-11-11 21:34:57 +1100

    Basic JavaScript: Understand String Immutability

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Basic JavaScript: Use Bracket Notation to Find the First Character in a String
-// Bracket notation is a way to get a character at a specific index within a string
-// JavaScript, start counting at 0 which is called Zero-based indexing
+// Basic JavaScript:  Understand String Immutability
+// String values are immutable
+// Means that they cannot be altered once created.
+// Individual characters of a string literal cannot be changed
+// The only way to change myStr would be to assign it with a new string
 
-var firstLetterOfLastName = "";
-var lastName = "Lovelace";
+var myStr = "Jello World";
 
-firstLetterOfLastName = lastName[0];
-console.log(firstLetterOfLastName);
+myStr = "Hello World";
+console.log(myStr);
