73efbe81a02042300a002578b869675917b32744
Janice Leung <janice.leung2008@gmail.com>
2020-11-14 23:27:54 +1100

    Basic JavaScript: Access Multi-Dimensional Arrays With Indexes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Basic JavaScript: Modify Array Data With Indexes
-// Entries of arrays are mutable and can be changed freely
-// Shouldn't be any spaces between the array name and the square brackets
+// Basic JavaScript: Access Multi-Dimensional Arrays With Indexes
+// Using brackets to access your array,
+// The first set of brackets refers to the entries in the outer-most (the first level) array
+// Each additional pair of brackets refers to the next level of entries inside.
 
-var myArray = [18,64,99];
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 
-myArray[0] = 45;
+var myData = myArray[2][1];
 
-console.log(myArray[0]);
+console.log(myData);
