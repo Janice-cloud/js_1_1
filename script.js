5ca90986bb47995ebb6879a461fd185130ba8ace
Janice Leung <janice.leung2008@gmail.com>
2020-11-14 23:23:19 +1100

    Basic JavaScript: Modify Array Data With Indexes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Basic JavaScript: Access Array Data with Indexes
-// Access the data inside arrays using indexes
-// Array indexes are written in the same bracket notation that strings use
-// Except that instead of specifying a character, they are specifying an entry in the array
-// Use zero-based indexing, so the first element in an array has an index of 0
+// Basic JavaScript: Modify Array Data With Indexes
+// Entries of arrays are mutable and can be changed freely
+// Shouldn't be any spaces between the array name and the square brackets
 
-var myArray = [50,60,70];
+var myArray = [18,64,99];
 
-var myData = myArray[0];
+myArray[0] = 45;
 
 console.log(myArray[0]);
