148383c180d9a93b74c693efe171db929820fcc7
Janice Leung <janice.leung2008@gmail.com>
2020-11-14 23:20:36 +1100

    Basic JavaScript: Access Array Data with Indexes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Basic JavaScript: Nest one Array within Another Array
-// Multi-dimensional array : nest arrays within other arrays
+// Basic JavaScript: Access Array Data with Indexes
+// Access the data inside arrays using indexes
+// Array indexes are written in the same bracket notation that strings use
+// Except that instead of specifying a character, they are specifying an entry in the array
+// Use zero-based indexing, so the first element in an array has an index of 0
 
-var myArray = [["Bulls", 23], ["White Sox", 45]];
+var myArray = [50,60,70];
 
-console.log(myArray[0], myArray[1]);
+var myData = myArray[0];
+
+console.log(myArray[0]);
