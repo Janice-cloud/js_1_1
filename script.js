8773f631fc86dc304dec992d6139aed4f8cac08d
Janice Leung <janice.leung2008@gmail.com>
2020-11-15 12:39:01 +1100

    Basic JavaScript: Stand in Line

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// Basic JavaScript: Assignment with a Returned Value
-// Storing Values with the Assignment Operator (=)
-// Everything to the right of the equal sign is resolved before the value is assigned
-// Meaning we can take the return value of a function and assign it to a variable.
+// Basic JavaScript: Stand in Line
+// In Computer Science a queue is an abstract Data Structure where items are kept in order.
+// New items can be added at the back of the queue and old items are taken off from the front of the queue.
 
-var processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
+function nextInLine(arr, item) {
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;
 }
 
-processed = processArg(7);
+testArr = [1,2,3,4,5]; 
 
-console.log(processed); // 2
+console.log("Before: " + testArr); // Before: 1,2,3,4,5
+
+console.log(nextInLine(testArr, 6)); // 1
+
+console.log("After: " + JSON.stringify(testArr)); // After: [2,3,4,5,6]
