c754b84cd2f63a76c64a61d04a37bab1df9028ef
Janice Leung <janice.leung2008@gmail.com>
2020-11-15 12:57:13 +1100

    Basic JavaScript: Comparison with the Strict Equality Operator

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Basic JavaScript: Comparison with the Equality Operator
-// Comparison operators in JavaScript - All of these operators return a boolean true or false value.
-// Most basic operator is the equality operator ==
-// The equality operator compares two values and returns true if they're equivalent or false if they are not.
-// Type Coercion : For JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another.
+// Basic JavaScript: Comparison with the Strict Equality Operator
+// Strict equality (===) is the counterpart to the equality operator (==).
+// ==  equality operator, which attempts to convert both values being compared to a common type
+// === strict equality operator does not perform a type conversion.
+// If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
 
 // if condition is true or false statement is executed
 
-function testEqual(val) {
-  if (val == 12) {
+function testStrict(val) {
+  if (val === 7) {
     return "Equal";
   }
   return "Not Equal";
 }
 
-console.log(testEqual(10));
+console.log(testStrict("7"));
