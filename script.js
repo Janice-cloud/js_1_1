1c369b43525dab6c8278be3072e5ede655219ce2
Janice Leung <janice.leung2008@gmail.com>
2020-11-15 12:53:04 +1100

    Basic JavaScript: Comparison with the Equality Operator

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Basic JavaScript: Use Conditional Logic with If Statements
-// If statements are used to make decisions in code.
-// The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses.
-// These conditions are known as Boolean conditions and they may only be true or false.
-// When the condition evaluates to :
-// true :  the program executes the statement inside the curly braces.
-// false : the statement inside the curly braces will not execute.
+// Basic JavaScript: Comparison with the Equality Operator
+// Comparison operators in JavaScript - All of these operators return a boolean true or false value.
+// Most basic operator is the equality operator ==
+// The equality operator compares two values and returns true if they're equivalent or false if they are not.
+// Type Coercion : For JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another.
 
 // if condition is true or false statement is executed
 
-function trueOrFalse(wasThatTrue) {
-  if (wasThatTrue) {
-    return "Yes, that was true";
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
   }
-  return "No, that was false";
+  return "Not Equal";
 }
 
-console.log(trueOrFalse(true)); // Yes, that was true
-console.log(trueOrFalse(false)); // No, that was false
+console.log(testEqual(10));
