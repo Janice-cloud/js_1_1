c161a1228ff32cf390af9e522d879e749c700c14
Janice Leung <janice.leung2008@gmail.com>
2020-11-16 13:49:31 +1100

    Basic JavaScript: Comparison with the Inequality Operator

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Basic JavaScript: Practice comparing different values
-// == equality operator will perform a type conversion, and then evaluate the values
-// === strict equality operator will compare both the data type and value as-is, without converting one type to the other
-// typeof : Used to determine the type of a variable or a value
+// Basic JavaScript: Comparison with the Inequality Operator
+// != means "Not Equal" and returns false
+// inequality operator will convert data types of values while comparing.
 
-function compareEquality(a, b) {
-  if (a === b) {
-    return "Equal";
+function testNotEqual(val) {
+  if (val != 99) {
+    return "Not Equal";
   }
-  return "Not Equal";
+  return "Equal";
 }
 
-console.log(compareEquality(10, "10"));
-console.log(typeof 10);
-console.log(typeof "10");
+console.log(testNotEqual(10));
