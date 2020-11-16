28ad897cf84946cf008f68ca5a1cb4afe7afe4f5
Janice Leung <janice.leung2008@gmail.com>
2020-11-16 14:28:20 +1100

    Basic JavaScript: Comparison with the Logical OR Operator

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Basic JavaScript: Comparisons with the Logical And Operator
-// && means logical and operator : testing more than one thing at a time
-// Returns true if and only if the operands to the left and right of it are true.
+// Basic JavaScript: Comparisons with the Logical Or Operator
+// || means logical OR operator : testing more than one thing at a time
+// Returns true if either of the operands is true
 // Otherwise, it returns false.
 
-function testLogicalAnd(val) {
-  if (val <= 50 && val >= 25) {
-    return "Yes";
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
   }
 
-  return "No";
+  return "Inside";
 }
 
-console.log(testLogicalAnd(10));
+console.log(testLogicalOr(15));
