6603e5cd4b3ca94487b48107a5269b43c5d448bf
Janice Leung <janice.leung2008@gmail.com>
2020-11-17 15:23:14 +1100

    Basic JavaScript: Accessing Object Properties with Dot Notation

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Basic JavaScript: Build JavaScript Objects
-// Objects : access the data in objects via properties.
-// Objects are useful for storing data in a structured way, and can represent real world objects.
-// If your object has any non-string properties, JavaScript will automatically typecast them as strings.
+// Basic JavaScript: Accessing Object Properties with Dot Notation
+// There are two ways to access the properties of an object: 
+// dot notation (.) - is what you use when you know the name of the property you're trying to access ahead of time.
+// bracket notation ([]) similar to an array.
 
-var myDog = {
-  name: "Pepper",
-  legs: 3,
-  tails: 0,
-  friends: ["Mac", "Poppy", "Daisy", "Ben"],
-};
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  var hatValue = testObj.hat;      
+  var shirtValue = testObj.shirt;    
+
+  console.log("My favourite type of hat is a " + hatValue + ". My favourite shirt is a " + shirtValue);
+  
