a0b1b400a120ad0cd20a11a82f319c67fdce9174
Janice Leung <janice.leung2008@gmail.com>
2020-11-17 13:30:06 +1100

    Basic JavaScript: Golf Code

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// Basic JavaScript:  Chaining If Else Statements
-// if/else statements can be chained together for complex logic. 
-// Here is example of multiple chained if / else if statements:
+// Basic JavaScript:  Golf Code
+// Game of golf each hole has a par meaning the average number of strokes a golfer is expected to make in order to sink the ball in a hole to complete the play. 
+// Depending on how far above or below par your strokes are, there is a different nickname.
+// Function will be passed par and strokes arguments. Return the correct string according the strokes in order of priority; top (highest) to bottom (lowest).
 
-function testSize(num) {
-    if ( num < 5 ) {
-      return "Tiny";
-    } else if ( num < 10 ) {
-      return "Small";
-    } else if ( num < 15 ) {
-      return "Medium";
-    } else if ( num < 20 ) {
-      return "Large";
-    } else if ( num >= 20 ) {
-      return "Huge";
-    } else {
+var names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  } else {
     return "Change Me";
-    }
   }
-  
-  console.log(testSize(7));
+}
+
+console.log(golfScore(5, 4));
