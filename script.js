4bc68f11e2290a5fbf919b2bfd185a29a64b0f2b
Janice Leung <janice.leung2008@gmail.com>
2020-11-17 15:12:07 +1100

    Basic JavaScript: Counting Cards

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-// Basic JavaScript:  Return Early Pattern for Functions
-// When return statement is reached, the execution of the current function stops and control returns to the calling location.
+// Basic JavaScript:  Counting Cards
+// In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+// Write card counting function that :
+// Receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value.
+// Return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative.
 
-function abTest(a, b) {
-  if (a < 0 || b < 0) {
-    return undefined;
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
   }
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
 }
 
-console.log(abTest(-1, 2));
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("J");
+cc("Q");
+console.log(cc());
