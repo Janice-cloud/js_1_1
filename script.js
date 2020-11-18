94e8064469f3be63fc8f8e64ae94b6e83ca56531
Janice Leung <janice.leung2008@gmail.com>
2020-11-18 11:44:25 +1100

    Basic JavaScript: Updating Object Properties

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Basic JavaScript: Accessing Object Properties with Variables
-// Another use of bracket notation on objects is to access a property which is stored as the value of a variable.
-// Useful for iterating through an object's properties or when accessing a lookup table
-// Do not use quotes around the variable name when using it to access the property because we are using the value of the variable, not the name
+// Basic JavaScript: Updating Object Properties
+// After creating a JavaScript object, its properties can be up dated at any time just like you would update any other variable
 
-var testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas",
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
 };
 
-var playerNumber = 16;
-var player = testObj[playerNumber];
+myDog.name = "Happy Coder";
 
-console.log(player); // Montana
+console.log(myDog.name);
