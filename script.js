2bf0bb3463846c7be387be0c0cbeb7b490d34827
Janice Leung <janice.leung2008@gmail.com>
2020-11-19 09:50:47 +1100

    Basic JavaScript: Iterate Through an Array with a For Loop

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// Basic JavaScript: Count Backwards With a For Loop
-// For loop can also count backwards, so long as the right conditions are defined.
-// To count backwards by twos, need to change the initialization, condition, and final-expression
-// Start at i = 10 and loop while i > 0. Will decrement i by 2 each loop with i -= 2
+// Basic JavaScript: Iterate Through an Array with a For Loop
+// Common task in JS is to iterate through the contents of an array.
+// One way to do that is with a for loop.
 
-var evenArray = [];
-for (var i = 10; i > 0; i -= 2) {
-  evenArray.push(i);
+// Iterating through an Array
+var arr = [10, 9, 8, 7, 6];
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
 
-console.log(evenArray); // count by even numbers
+// adding numbers in an Array
+var myArr = [2, 3, 4, 5, 6];
 
-var oddArray = [];
-
-for (var i = 9; i > 0; i -= 2) {
-  oddArray.push(i);
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  console.log((total += myArr[i]));
 }
-
-console.log(oddArray); // count by odd numbers
