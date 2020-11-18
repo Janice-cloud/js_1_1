228914e41db832dd75b9cb520c6c9d75f987ad9e
Janice Leung <janice.leung2008@gmail.com>
2020-11-18 11:56:48 +1100

    Basic JavaScript: Using Objects for Lookups

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// Basic JavaScript: Delete Properties from a JavaScript Object
-// Properties can be deleted from Objects
+// Basic JavaScript: Using Objects for Lookups
+// Objects can be thought of as a key/value storage, like a dictionary.
+// If you have tabular data, you can use an object to "lookup" values rather than a switch statement or an if/else chain.
+// This is most useful when you know that your input data is limited to a certain range.
 
-var myDog = {
-  "name": "Happy Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"],
-  "bark": "woof"
-};
+function phoneticLookup(val) {
+  var result = "";
 
-delete myDog.tails;
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
 
-console.log(myDog);
+  return result;
+}
+
+console.log(phoneticLookup("charlie"));
