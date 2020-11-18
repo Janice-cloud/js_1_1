57204242724c8e5b94696c22c5d34263f24361b0
Janice Leung <janice.leung2008@gmail.com>
2020-11-19 09:22:44 +1100

    Basic JavaScript: Iterate Odd and even Numbers With a For Loop

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Basic JavaScript: Iterate with JavaScript For Loops
-// The same code can be run multiple times by using a loop.
-// For loop - runs "for" a specific number of times.
-// For loops are declared with three optional expressions separated by semicolons:
-// for ([initialization]; [condition]; [final-expression])
-// - Initialization statement is executed one time only before the loop starts. It is typically used to define and setup the loop variable.
-// - Condition statement is evaluated at the beginning of every loop iteration and will continue as long as it evaluates to true. When condition is false at the start of the iteration, the loop will stop executing. This means if condition starts as false, the loop will never execute.
-// - Final-expression is executed at the end of each loop iteration, prior to the next condition check and is usually used to increment or decrement your loop counter.
+// Basic JavaScript: Iterate Odd Numbers With a For Loop
+// For loops don't have to iterate one at a time
+// Changing the final-expression, loop can count by even numbers
+// Start at i = 0 and loop while i < 10. To increment i by 2 each loop by using i += 2.
 
-var myArray = [];
+var evenArray = [];
 
-for (var i = 1; i < 6; i++) {
-  myArray.push(i);
+for (var i = 0; i < 10; i += 2) {
+  evenArray.push(i);
 }
 
-console.log(myArray);
+console.log(evenArray); // count by even numbers
+
+var oddArray = [];
+
+for (var i = 1; i < 10; i += 2) {
+  oddArray.push(i);
+}
+
+console.log(oddArray); // count by odd numbers
