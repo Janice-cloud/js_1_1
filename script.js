de6cf21a2d64a1f222231b6596557ea7185e6830
Janice Leung <janice.leung2008@gmail.com>
2020-11-19 09:16:08 +1100

    Basic JavaScript: Iterate with JavaScript For Loops

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Basic JavaScript: Iterate with JavaScript While Loops
+// Basic JavaScript: Iterate with JavaScript For Loops
 // The same code can be run multiple times by using a loop.
-// While loop - runs "while" a specified condition is true and stops once that condition is no longer true.
+// For loop - runs "for" a specific number of times.
+// For loops are declared with three optional expressions separated by semicolons:
+// for ([initialization]; [condition]; [final-expression])
+// - Initialization statement is executed one time only before the loop starts. It is typically used to define and setup the loop variable.
+// - Condition statement is evaluated at the beginning of every loop iteration and will continue as long as it evaluates to true. When condition is false at the start of the iteration, the loop will stop executing. This means if condition starts as false, the loop will never execute.
+// - Final-expression is executed at the end of each loop iteration, prior to the next condition check and is usually used to increment or decrement your loop counter.
 
 var myArray = [];
 
-var i = 0;
-while (i < 6) {
-  myArray.unshift(i);
-  i++;
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
 }
 
 console.log(myArray);
