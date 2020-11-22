4a612a3bccd0c91dd8601f4c300f6f63958ece7b
Janice Leung <janice.leung2008@gmail.com>
2020-11-22 21:53:09 +1100

    Basic JavaScript: Use Multiple Conditional (Ternary) Operators

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Basic JavaScript: Use the Conditional (Ternary) Operator
-// conditional operator, also called the ternary operator, can be used as a one line if-else expression.
-// Syntax : condition ? expression-if-true : expression-if-false
+// Basic JavaScript: Use Multiple Conditional (Ternary) Operators
+// Conditional operators can be chained together to check for multiple conditions.
+// Following function uses if, else if, and else statements to check multiple conditions:
 
-function checkEqual(a, b) {
-  return a === b ? "Equal" : "Not Equal";
+function checkSign(num) {
+  return num === 0 ? "zero" : num > 0 ? "positive" : "negative";
 }
 
-console.log(checkEqual(1, 2));
+console.log(checkSign(10));
