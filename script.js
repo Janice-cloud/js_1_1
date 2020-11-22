68e929a82ea9fc0d33996384ef8349631a8191f1
Janice Leung <janice.leung2008@gmail.com>
2020-11-22 21:31:48 +1100

    Basic JavaScript: Generate Random Whole Numbers within a Range

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Basic JavaScript: Generate Random Whole Numbers with JavaScript
-// How to generate a randome whole number
-// 1. Use Math.random() to generate a random decimal.
-// 2. Multiply that random decimal by 20.
-// 3. Use another function, Math.floor() to round the number down to its nearest whole number.
-// Math.random() can never quite return a 1 and, because we're rounding down, it's impossible to actually get 20. This technique will give us a whole number between 0 and 19.
-// Example of code :  Math.floor(Math.random() * 20);
+// Basic JavaScript: Generate Random Whole Numbers within a Range
+// Can generate a random number between two specific numbers.
+// Define a minimum number min and a maximum number max
+// Example code :  Math.floor(Math.random() * (max - min + 1)) + min;
 
-function randomWholeNum() {
-  return Math.floor(Math.random() * 10);
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random()*(myMax - myMin +1)) + myMin;
 }
 
-console.log(randomWholeNum());
+
+console.log(randomRange(20, 30));
