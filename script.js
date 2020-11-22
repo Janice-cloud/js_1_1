0ce52aa4541194fdc47e5cb83c0e83a352a81c55
Janice Leung <janice.leung2008@gmail.com>
2020-11-22 21:48:00 +1100

    Basic JavaScript: Use the Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Basic JavaScript: Use the parseInt Function with a Radix
-// parseInt() function parses a string and returns an integer
-// It takes a second argument for the radix, which specifies the base of the number in the string.
-// The radix can be an integer between 2 and 36.
-// Function call looks like :  parseInt(string, radix);
-// Example code : var a = parseInt("11", 2);
-// The radix variable says that "11" is in the binary system, or base 2. This example converts the string "11" to an integer 3.
+// Basic JavaScript: Use the Conditional (Ternary) Operator
+// conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+// Syntax : condition ? expression-if-true : expression-if-false
 
-function convertToInteger(str) {
-  return parseInt(str, 2);
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
 }
 
-console.log(convertToInteger("10011"));
+console.log(checkEqual(1, 2));
