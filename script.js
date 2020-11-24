3218f8c8a2b17cd3410d7d3c234c1d551be87e11
Janice Leung <janice.leung2008@gmail.com>
2020-11-24 15:50:27 +1100

    Basic JavaScript: Use Recursion to Create a Countdown

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// Basic JavaScript: Use Multiple Conditional (Ternary) Operators
-// Conditional operators can be chained together to check for multiple conditions.
-// Following function uses if, else if, and else statements to check multiple conditions:
+// Basic JavaScript: Use Recursion to Create a Countdown
+// Complex function that returns an array of consecutive integers starting with 1 through the number passed to the function.
 
-function checkSign(num) {
-  return num === 0 ? "zero" : num > 0 ? "positive" : "negative";
+
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
 }
+console.log(countup(5));
 
-console.log(checkSign(10));
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+  return;
+}
+console.log(countdown(10));
