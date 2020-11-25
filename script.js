fc1494fabb2848afdec865a2dd7f942bf975be5c
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 10:22:44 +1100

    ES6: Use the Rest Parameter with Function Parameters

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// ES6: Set Default Parameters for Your Functions
-// In order to help us create more flexible functions, ES6 introduces default parameters for functions.
+// ES6: Use the Rest Parameter with Function Parameters
+// rest parameter - can create functions that take a variable number of arguments. 
+// These arguments are stored in an array that can be accessed later from inside the function.
 
-const greeting = (name = "Anonymous") => "Hello " + name;
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2)); // You have passed 3 arguments.
+console.log(howMany("string", null, [1, 2, 3], { })); // You have passed 4 arguments.
 
-console.log(greeting("John")); // Hello John
-console.log(greeting()); // Hello Anonymous
+//The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array. 
 
-//The default parameter kicks in when the argument is not specified (it is undefined).
-//You can add default values for as many parameters as you want.
-
-// function that increment by adding default parameters so that it will add 1 to number if value is not specified.
-const increment = (number, value = 1) => number + value;
-console.log(increment(0));
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(2, 4, 6,));
