2f6dbe73952f30233ba21a728337e9fd77a823c3
Janice Leung <janice.leung2008@gmail.com>
2020-11-25 20:19:34 +1100

    ES6: Declare a Read-Only Variable with the const Keyword

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// ES6: Compare Scopes of the var and let Keywords
-// Declaring a variable with the var keyword, it is declared globally, or locally if declared inside a function.
-// let keyword behaves similarly, but with some extra features
-// when let keyword is declared inside a block, statement, or expression, its scope is limited to that block, statement, or expression.
+// ES6: Declare a Read-Only Variable with the const Keyword
+// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only.
+// They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned.
+// trying to reassign a variable declared with const will throw an error
+// common practice when naming constants is to use all uppercase letters, with words separated by an underscore.
+// use uppercase variable identifiers for immutable values
+// use lowercase or camelCase for mutable values (objects and arrays)
 
-function checkScope() {
-  'use strict';
-  let i = 'function scope';
-  if (true) {
-    let i = 'block scope';
-    console.log('Block scope i is: ', i);
+function printManyTimes(str) {
+  "use strict";
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
   }
-  console.log('Function scope i is: ', i);
-  return i;
 }
-
-console.log(checkScope());
+printManyTimes("freeCodeCamp");
