c24c4880348defb34d659731178adcf78087c817
Janice Leung <janice.leung2008@gmail.com>
2020-11-25 20:07:18 +1100

    ES6: Explore Differences Between the var and let Keywords

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Basic JavaScript: Use Recursion to Create a Range of Numbers
-// The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. 
-// The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+// ES6: Explore Differences Between the var and let Keywords
+// var - can overwrite variable declarations without an error. Which is a problem in larger code by accidentally overwriting a variable that was not intended to be overwriten.
+// As this behavior does not throw an error searching and fixing bugs becomes more difficult
+// let - solves this potential issue. Which, if a let variable is declared more than once than error of "use strict" will catch the error.
 
-function rangeOfNumbers(startNum, endNum) {
-  if (endNum - startNum === 0) {
-   return [startNum];
- } else {
-   var numbers = rangeOfNumbers(startNum, endNum - 1);
-   numbers.push(endNum);
-   return numbers;
- }
-};
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
 
-console.log(rangeOfNumbers(10, 20));
+  catName = "Oliver";
+  quote = catName + " says Meow!";
 
-console.log(rangeOfNumbers(10, 10));
+}
+console.log(catTalk());
