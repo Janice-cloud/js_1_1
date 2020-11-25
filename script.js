1d547c2ebc8f31b916a60d16bf2099e19584dfa3
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 10:58:01 +1100

    ES6: Use Destructuring Assignment to Extract Values from Objects

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// ES6: Use the Spread Operator to Evaluate Arrays In-Place
-// spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected.
+// ES6: Use Destructuring Assignment to Extract Values from Objects
+// Destructuring assignment is special syntax introduced in ES6, for neatly assigning values taken directly from an object.
 
-// ES5 code below uses apply() to compute the maximum value in an array:
-// Math.max.apply(null, arr) needed to be used because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array
+// The following ES5 code:
 
-var arr = [6, 89, 3, 45];
-var maximus = Math.max.apply(null, arr); // returns 89
+/*const user = { name: 'John Doe', age: 34 };
+const name = user.name; // name = 'John Doe'
+const age = user.age; // age = 34*/
 
-//  spread operator makes this syntax much better to read and maintain
+// An equivalent assignment statement using the ES6 destructuring syntax:
 
-const arr = [6, 89, 3, 45];
-const maximus = Math.max(...arr); // returns 89
+//const { name, age } = user;
+// name = 'John Doe', age = 34
 
-// ...arr returns an unpacked array. It spreads the array. The spread operator only works in-place, like in an argument to a function or in an array literal. The following code will not work:
+// Here, the name and age variables will be created and assigned the values of their respective values from the user object.
+// You can extract as many or few values from the object as you want.
 
-//const spreaded = ...arr; // will throw a syntax error
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+};
 
-var arr = [6, 89, 3, 45];
-var maximus = Math.max.apply(null, arr); // returns 89
+const { today, tomorrow } = HIGH_TEMPERATURES;
 
-const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
-let arr2;
-
-arr2 = [...arr1];
-
-console.log(arr2);
+console.log(today);
+console.log(tomorrow);
