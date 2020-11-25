7b6f3d9ca5435682d59ee5264fb2aa0215b89ff5
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 10:35:29 +1100

    Use the Spread Operator to Evaluate Arrays In-Place

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-// ES6: Use the Rest Parameter with Function Parameters
-// rest parameter - can create functions that take a variable number of arguments. 
-// These arguments are stored in an array that can be accessed later from inside the function.
+// ES6: Use the Spread Operator to Evaluate Arrays In-Place
+// spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected.
 
-function howMany(...args) {
-  return "You have passed " + args.length + " arguments.";
-}
-console.log(howMany(0, 1, 2)); // You have passed 3 arguments.
-console.log(howMany("string", null, [1, 2, 3], { })); // You have passed 4 arguments.
+// ES5 code below uses apply() to compute the maximum value in an array:
+// Math.max.apply(null, arr) needed to be used because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array
 
-//The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array. 
+var arr = [6, 89, 3, 45];
+var maximus = Math.max.apply(null, arr); // returns 89
 
-const sum = (...args) => {
-  return args.reduce((a, b) => a + b, 0);
-}
-console.log(sum(2, 4, 6,));
+//  spread operator makes this syntax much better to read and maintain
+
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr); // returns 89
+
+// ...arr returns an unpacked array. It spreads the array. The spread operator only works in-place, like in an argument to a function or in an array literal. The following code will not work:
+
+//const spreaded = ...arr; // will throw a syntax error
+
+var arr = [6, 89, 3, 45];
+var maximus = Math.max.apply(null, arr); // returns 89
+
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+
+arr2 = [...arr1];
+
+console.log(arr2);
