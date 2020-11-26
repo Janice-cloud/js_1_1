fbd66a08d5feb6d011cb22070e9e25f6e32354f4
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 11:28:49 +1100

    ES6: Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// ES6: Use Destructuring Assignment to Assign Variables from Arrays
-// ES6 makes destructuring arrays as easy as destructuring objects.
-// Key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list.
-// Therefore you cannot pick or choose which elements you want to assign to variables.
+// ES6: Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+// some situations involving array destructuring, we might want to collect the rest of the elements into a separate array.
+// The result is similar to Array.prototype.slice(), as shown below:
 
-// Destructuring an array lets us do exactly that:
+/*const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b); // 1, 2
+console.log(arr); // [3, 4, 5, 7]*/
 
-/*const [a, b] = [1, 2, 3, 4, 5, 6];
-console.log(a, b); // 1, 2*/
+// Variables a and b take the first and second values from the array.
+// After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array.
+// The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array.
 
-//The variable a is assigned the first value of the array, and b is assigned the second value of the array. We can also access the value at any index in an array with destructuring by using commas to reach the desired index:
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  "use strict";
+  const [a, b, ...arr] = list;
+  return arr;
+}
+const arr = removeFirstTwo(source);
 
-/*const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, c); // 1, 2, 5*/
-
-// Swapping the values using destructing assignment
-let a = 8,
-  b = 6;
-
-[a, b] = [b, a];
-
-console.log(a, b);
+console.log(arr); // 3, 4, 5, 6, 7, 8, 9, 10
