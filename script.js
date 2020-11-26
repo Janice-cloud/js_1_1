25e8674229a2ac31a605d5a3561176a88a04ed9a
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 11:37:16 +1100

    ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-// ES6: Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
-// some situations involving array destructuring, we might want to collect the rest of the elements into a separate array.
-// The result is similar to Array.prototype.slice(), as shown below:
+// ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// destructure the object in a function argument itself.
 
-/*const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
-console.log(a, b); // 1, 2
-console.log(arr); // [3, 4, 5, 7]*/
-
-// Variables a and b take the first and second values from the array.
-// After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array.
-// The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array.
-
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function removeFirstTwo(list) {
-  "use strict";
-  const [a, b, ...arr] = list;
-  return arr;
+/*
+const profileUpdate = (profileData) => {
+const { name, age, nationality, location } = profileData;
+  // do something with these variables
 }
-const arr = removeFirstTwo(source);
 
-console.log(arr); // 3, 4, 5, 6, 7, 8, 9, 10
+This effectively destructures the object sent into the function. This can also be done in-place:
+
+const profileUpdate = ({ name, age, nationality, location }) => {
+  // do something with these fields 
+}
+
+When profileData is passed to the above function, the values are destructured from the function parameter for use within the function
+*/
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85,
+};
+
+const half = ({ max, min }) => (max + min) / 2.0;
+
+console.log(half(stats));
