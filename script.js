59a36b8cbec377a09cd3a158683f8a3815c3d67d
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 12:02:47 +1100

    ES6: Write Concise Object Literal Declarations Using Object Property

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-// ES6: Create Strings using Template Literals
-// template literal - special type of string that makes creating complex strings easier.
-// Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+// ES6: Write Concise Object Literal Declarations Using Object Property Shorthand
+// Consider the following code:
 
-const person = {
-  name: "Zodiac Hasbro",
-  age: 56,
+const getMousePosition = (x, y) => ({
+  x: x,
+  y: y,
+});
+
+// getMousePosition is a simple function that returns an object containing two properties
+//  ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood.
+// Here is the same function from above rewritten to use this new syntax:
+
+//const getMousePosition = (x, y) => ({ x, y });
+
+//Example :
+
+/*const createPerson = (name, age, gender) => {
+  "use strict";
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // Only change code above this line
+};*/
+
+// Using object property shorthand with object literals to create and return an object with name, age and gender properties.
+
+const createPerson = (name, age, gender) => {
+  return { name, age, gender };
 };
 
-// Template literal with multi-line and string interpolation
-const greeting = `Hello, my name is ${person.name}!
-I am ${person.age} years old.`;
-
-console.log(greeting); // prints
-// Hello, my name is Zodiac Hasbro!
-// I am 56 years old.
-
-// First, the example uses backticks (`), not quotes (' or "), to wrap the string.
-// Second, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings.
-// The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore.
-// To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }.
-// Similarly, you can include other expressions in your string literal, for example ${a + b}.
-
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"],
-};
-function makeList(arr) {
-  const failureItems = [];
-  for (let i = 0; i < arr.length; i++) {
-    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
-  }
-
-  return failureItems;
-}
-
-const failuresList = makeList(result.failure);
-
-console.log(failuresList)
+console.log(createPerson("Joe", 15, "Male"));
