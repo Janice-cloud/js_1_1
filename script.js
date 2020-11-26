078a31dbffe9847a1356e36b693e01ee9767f8cf
Janice Leung <janice.leung2008@gmail.com>
2020-11-26 11:54:41 +1100

    ES6: Create Strings using Template Literals

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-// ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
-// destructure the object in a function argument itself.
+// ES6: Create Strings using Template Literals
+// template literal - special type of string that makes creating complex strings easier.
+// Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
 
-/*
-const profileUpdate = (profileData) => {
-const { name, age, nationality, location } = profileData;
-  // do something with these variables
-}
-
-This effectively destructures the object sent into the function. This can also be done in-place:
-
-const profileUpdate = ({ name, age, nationality, location }) => {
-  // do something with these fields 
-}
-
-When profileData is passed to the above function, the values are destructured from the function parameter for use within the function
-*/
-
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85,
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56,
 };
 
-const half = ({ max, min }) => (max + min) / 2.0;
+// Template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
 
-console.log(half(stats));
+console.log(greeting); // prints
+// Hello, my name is Zodiac Hasbro!
+// I am 56 years old.
+
+// First, the example uses backticks (`), not quotes (' or "), to wrap the string.
+// Second, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings.
+// The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore.
+// To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }.
+// Similarly, you can include other expressions in your string literal, for example ${a + b}.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+function makeList(arr) {
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList)
