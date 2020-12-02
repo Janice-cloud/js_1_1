856d75a63f7a8312f9cf65ed03b711a0d25d78f2
Janice Leung <janice.leung2008@gmail.com>
2020-12-02 13:48:41 +1100

    ES6: Create a Module Script

--- script.js
+++ script.js
@@ -1,46 +1,14 @@
-// ES6: Use getters and setters to Control Access to an Object
-// You can obtain values from an object and set the value of a property within an object.
-// These are classically called getters and setters.
-// Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
-// Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely.
+// ES6: Create a Module Script
+// In order to make JavaScript more modular, clean, and maintainable; ES6 introduced a way to easily share code among JavaScript files. This involves exporting parts of a file for use in one or more other files, and importing the parts you need, where you need them. In order to take advantage of this functionality, you need to create a script in your HTML document with a type of module. Here’s an example:
 
-/*class Book {
-  constructor(author) {
-    this._author = author;
-  }
-  // getter
-  get writer() {
-    return this._author;
-  }
-  // setter
-  set writer(updatedAuthor) {
-    this._author = updatedAuthor;
-  }
-}
-const novel = new Book('anonymous');
-console.log(novel.writer);  // anonymous
-novel.writer = 'newAuthor';
-console.log(novel.writer);  // newAuthor*/
+// <script type="module" src="filename.js"></script>
 
-// Getters and setters are important because they hide internal implementation details.
-//  Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
+// A script that uses this module type can now use the import and export
 
-class Thermostat {
-  constructor(fahrenheit) {
-    this.fahrenheit = fahrenheit;
-  }
-  get temperature() {
-    return (5 / 9) * (this.fahrenheit - 32);
-  }
-  set temperature(celsius) {
-    this.fahrenheit = (celsius * 9.0) / 5 + 32;
-  }
-}
+<html>
+  <body>
 
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature;
-console.log(temp.toFixed(2)); // 24.44 in Celsius
+<script type="module" src="index.js"></script>
 
-thermos.temperature = 26;
-temp = thermos.temperature;
-console.log(temp); // 26 in Celsius
+  </body>
+</html>
