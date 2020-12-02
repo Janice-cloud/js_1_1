93d52b6003e688b0e39728ebcd1a5402c346f349
Janice Leung <janice.leung2008@gmail.com>
2020-12-02 14:58:30 +1100

    ES6: Complete a Promise with resolve and reject

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-// ES6: Create a JavaScript Promise
+// ES6: Complete a Promise with resolve and reject
 /* 
 
-A promise in JavaScript is used to make a promise to do something, usually asynchronously.
+A promise has three states: pending, fulfilled, and rejected. 
 
-When the task completes, you either fulfill your promise or fail to do so.
+The promise you created in the last challenge is forever stuck in the pending state because you did not add a way to complete the promise. The resolve and reject parameters given to the promise argument are used to do this. 
 
-Promise is a constructor function, so you need to use the new keyword to create one. 
+Resolve is used when you want your promise to succeed.
+Reject is used when you want it to fail. 
 
-It takes a function, as its argument, with two parameters - resolve and reject.
-
-These are methods used to determine the outcome of the promise. The syntax looks like this:
+These are methods that take an argument, as seen below.
 
   const myPromise = new Promise((resolve, reject) => {
+    if(condition here) {
+      resolve("Promise was fulfilled");
+    } else {
+      reject("Promise was rejected");
+    }
   });
+
+The example above uses strings for the argument of these functions, but it can really be anything. Often, it might be an object, that you would use data from, to put on your website or elsewhere.
 
 */
 
 const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer = "7";
 
+  if (responseFromServer) {
+    resolve(console.log("We got the data"));
+  } else {
+    reject(console.log("Data not received"));
+  }
 });
-
