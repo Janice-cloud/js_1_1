323ea0727c8ae688c6352a9335974ea79f258d21
Janice Leung <janice.leung2008@gmail.com>
2020-12-02 14:12:01 +1100

    ES6: Reuse JavaScript Code Using import

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-// ES6: Use export to Share a Code Block 
-/* Imagine a file called math_functions.js that contains several functions related to mathematical operations. One of them is stored in a variable, add, that takes in two numbers and returns their sum. You want to use this function in several different JavaScript files. In order to share it with these other files, you first need to export it. 
+// ES6: Reuse JavaScript Code Using import
+/* import - allows you to choose which parts of a file or module to load. In the previous lesson, the examples exported add from the math_functions.js file. Here's how you can import it to use in another file:
 
-export const add = (x, y) => {
-  return x + y;
-}
+import { add } from './math_functions.js';
 
-The above is a common way to export a single function, but you can achieve the same thing like this:
+import will find add in math_functions.js, import just that function for you to use, and ignore the rest.
 
-const add = (x, y) => {
-  return x + y;
-}
+The ./ tells the import to look for the math_functions.js file in the same folder as the current file. The relative file path (./) and file extension (.js) are required when using import in this way.
 
-export { add };
+You can import more than one item from the file by adding them in the import statement like this:
 
-When you export a variable or function, you can import it in another file and use it without having to rewrite the code. You can export multiple things by repeating the first example for each thing you want to export, or by placing them all in the export statement of the second example, like this:
+import { add, subtract } from './math_functions.js';
 
-export { add, subtract }; */
+*/
 
-const uppercaseString = (string) => {
-  return string.toUpperCase();
-}
+import { uppercaseString, lowercaseString } from './string_functions.js'; 
 
-const lowercaseString = (string) => {
-  return string.toLowerCase()
-}
-
-export {uppercaseString, lowercaseString};
+uppercaseString("hello");
+lowercaseString("WORLD!");
