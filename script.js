fc008c7849fe09faed7a3d0cbebe5ddf1d191264
Janice Leung <janice.leung2008@gmail.com>
2020-12-02 13:43:54 +1100

    ES6: Use getters and setters to Control Access to an Object

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-// ES6: Use class Syntax to Define a Constructor Function
-// ES6 provides a new syntax to create objects, using the class keyword.
-//class syntax is just syntax and not a full-fledged class-based implementation of an object-oriented paradigm, unlike in languages such as Java, Python, Ruby, etc.
-//  ES5, we usually define a constructor function and use the new keyword to instantiate an object.
-/*var SpaceShuttle = function(targetPlanet){
-  this.targetPlanet = targetPlanet;
-}
-var zeus = new SpaceShuttle('Jupiter');*/
+// ES6: Use getters and setters to Control Access to an Object
+// You can obtain values from an object and set the value of a property within an object.
+// These are classically called getters and setters.
+// Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
+// Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely.
 
-// The class syntax simply replaces the constructor function creation:
-
-/*class SpaceShuttle {
-  constructor(targetPlanet) {
-    this.targetPlanet = targetPlanet;
+/*class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
   }
 }
-const zeus = new SpaceShuttle('Jupiter');*/
+const novel = new Book('anonymous');
+console.log(novel.writer);  // anonymous
+novel.writer = 'newAuthor';
+console.log(novel.writer);  // newAuthor*/
 
-// Class keyword declares a new function, to which a constructor is added.
-// Constructor is invoked when new is called to create a new object.
+// Getters and setters are important because they hide internal implementation details.
+//  Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
 
-// UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above.
-// The constructor method is a special method for creating and initializing an object created with a class. You will learn more about it in the Object Oriented Programming section of the JavaScript Algorithms And Data Structures Certification.
-
-class Vegetable {
-  constructor(name) {
-    this.name = name;
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
   }
 }
 
-const carrot = new Vegetable("carrot");
-console.log(carrot.name); // Should display 'carrot'
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature;
+console.log(temp.toFixed(2)); // 24.44 in Celsius
+
+thermos.temperature = 26;
+temp = thermos.temperature;
+console.log(temp); // 26 in Celsius
