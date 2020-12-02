17f4bdc6bb0452b18b472425e7f72340f1778ce1
Janice Leung <janice.leung2008@gmail.com>
2020-12-02 14:07:41 +1100

    ES6: Use export to Share a Code Block

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// ES6: Create a Module Script
-// In order to make JavaScript more modular, clean, and maintainable; ES6 introduced a way to easily share code among JavaScript files. This involves exporting parts of a file for use in one or more other files, and importing the parts you need, where you need them. In order to take advantage of this functionality, you need to create a script in your HTML document with a type of module. Here’s an example:
+// ES6: Use export to Share a Code Block 
+/* Imagine a file called math_functions.js that contains several functions related to mathematical operations. One of them is stored in a variable, add, that takes in two numbers and returns their sum. You want to use this function in several different JavaScript files. In order to share it with these other files, you first need to export it. 
 
-// <script type="module" src="filename.js"></script>
+export const add = (x, y) => {
+  return x + y;
+}
 
-// A script that uses this module type can now use the import and export
+The above is a common way to export a single function, but you can achieve the same thing like this:
 
-<html>
-  <body>
+const add = (x, y) => {
+  return x + y;
+}
 
-<script type="module" src="index.js"></script>
+export { add };
 
-  </body>
-</html>
+When you export a variable or function, you can import it in another file and use it without having to rewrite the code. You can export multiple things by repeating the first example for each thing you want to export, or by placing them all in the export statement of the second example, like this:
+
+export { add, subtract }; */
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString};
