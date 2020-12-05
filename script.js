dc644db32ef76611d7eece5664722a377fdd9aca
Janice Leung <janice.leung2008@gmail.com>
2020-12-06 07:26:25 +1100

    Regular Expressions: Match Literal Strings

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Regular Expressions: Using the Test Method
+// Regular Expressions: Match Literal Strings
 /* 
+In the last challenge, you searched for the word "Hello" using the regular expression /Hello/. That regex searched for a literal match of the string "Hello". Here's another example searching for a literal match of the string "Kevin":
 
-Regular expressions are used in programming languages to match parts of strings. You create patterns to help you do that matching.
-
-If you want to find the word "the" in the string "The dog chased the cat", you could use the following regular expression: /the/. Notice that quote marks are not required within the regular expression.
-
-JavaScript has multiple ways to use regexes. One way to test a regex is using the .test() method. The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), and returns true or false if your pattern finds something or not.
-
-let testStr = "freeCodeCamp";
-let testRegex = /Code/;
+let testStr = "Hello, my name is Kevin.";
+let testRegex = /Kevin/;
 testRegex.test(testStr);
 // Returns true
+Any other forms of "Kevin" will not match. For example, the regex /Kevin/ will not match "kevin" or "KEVIN".
 
+let wrongRegex = /kevin/;
+wrongRegex.test(testStr);
+// Returns false
+A future challenge will show how to match those other forms as well.
 */
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex.test(myString);
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; 
+let result = waldoRegex.test(waldoIsHiding);
 
-console.log(result); // True
+console.log(result);
