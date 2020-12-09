01212e9137c947e46ae83a94cdcf2672b07078c2
Janice Leung <janice.leung2008@gmail.com>
2020-12-09 16:29:17 +1100

    Regular Expressions: Match All Non-Numbers

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Regular Expressions: Match All Numbers
+// Regular Expressions: Match All Non-Numbers
 /* 
 
-You've learned shortcuts for common string patterns like alphanumerics. Another common pattern is looking for just digits or numbers.
+The last challenge showed how to search for digits using the shortcut \d with a lowercase d. You can also search for non-digits using a similar shortcut that uses an uppercase D instead.
 
-The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0-9], which looks for a single character of any number between zero and nine.
+The shortcut to look for non-digit characters is \D. This is equal to the character class [^0-9], which looks for a single character that is not a number between zero and nine.
 
 */
 
 let movieName = "2001: A Space Odyssey";
-let numRegex = /\d/g; 
+let numRegex = /\D/g; 
 let result = movieName.match(numRegex).length;
 
 
