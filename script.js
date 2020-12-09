0e8b5f54d2dca48e296f91af5309cf284a58b172
Janice Leung <janice.leung2008@gmail.com>
2020-12-09 15:58:28 +1100

    Regular Expressions: Match Beginning String Patterns

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Regular Expressions: Match Beginning String Patterns
 /* 
 
-Prior challenges showed that regular expressions can be used to look for a number of matches. They are also used to search for patterns in specific positions in strings.
+In the last challenge, you learned to use the caret character to search for patterns at the beginning of strings. There is also a way to search for patterns at the end of strings.
 
-In an earlier challenge, you used the caret character (^) inside a character set to create a negated character set in the form [^thingsThatWillNotBeMatched]. Outside of a character set, the caret is used to search for patterns at the beginning of strings.
+You can search the end of strings using the dollar sign character $ at the end of the regex.
 
-let firstString = "Ricky is first and can be found.";
-let firstRegex = /^Ricky/;
-firstRegex.test(firstString);
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding);
 // Returns true
-let notFirst = "You can't find Ricky now.";
-firstRegex.test(notFirst);
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding);
 // Returns false
 
 */
 
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/; 
-let result = calRegex.test(rickyAndCal);
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/;
+let result = lastRegex.test(caboose);
 
 console.log(result); // true
