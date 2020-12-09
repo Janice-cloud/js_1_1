9c5ccfc1e9d91f53ffa7d7012b29c2cc8329e804
Janice Leung <janice.leung2008@gmail.com>
2020-12-09 14:30:39 +1100

    Regular Expressions: Match Numbers and Letters of the Alphabet

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// Regular Expressions: Match Letters of the Alphabet
+// Regular Expressions: Match Numbers and Letters of the Alphabet
 /* 
 
-You saw how you can use character sets to specify a group of characters to match, but that's a lot of typing when you need to match a large range of characters (for example, every letter in the alphabet). Fortunately, there is a built-in feature that makes this short and simple.
+Using the hyphen (-) to match a range of characters is not limited to letters. It also works to match a range of numbers.
 
-Inside a character set, you can define a range of characters to match using a hyphen character: -.
+For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
 
-For example, to match lowercase letters a through e you would use [a-e].
+Also, it is possible to combine a range of letters and numbers in a single character set.
 
-let catStr = "cat";
-let batStr = "bat";
-let matStr = "mat";
-let bgRegex = /[a-e]at/;
-catStr.match(bgRegex); // Returns ["cat"]
-batStr.match(bgRegex); // Returns ["bat"]
-matStr.match(bgRegex); // Returns null
+let jennyStr = "Jenny8675309";
+let myRegex = /[a-z0-9]/ig;
+// matches all letters and numbers in jennyStr
+jennyStr.match(myRegex);
 
 */
 
-let quoteSample = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /[a-z]/gi; 
-let result = quoteSample.match(alphabetRegex); 
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; 
+let result = quoteSample.match(myRegex);
 
-console.log(result); // (35) ["T", "h", "e", "q", "u", "i", "c", "k", "b", "r", "o", "w", "n", "f", "o", "x", "j", "u", "m", "p", "s", "o", "v", "e", "r", "t", "h", "e", "l", "a", "z", "y", "d", "o", "g"]
+console.log(result); // (17) ["l", "r", "r", "3", "4", "5", "2", "6", "5", "3", "s", "r", "l", "i", "i", "o", "s"]
