8d5ba433ba6f16aea059376b06378869bfc38d6a
Janice Leung <janice.leung2008@gmail.com>
2020-12-09 14:39:00 +1100

    Regular Expressions: Match Characters that Occur One or More Times

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// Regular Expressions: Match Single Characters Not Specified
+// Regular Expressions: Match Characters that Occur One or More Times
 /* 
 
-So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
 
-To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
 
-For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+For example, /a+/g would find one match in "abc" and return ["a"]. Because of the +, it would also find a single match in "aabc" and return ["aa"].
+
+If it were instead checking the string "abab", it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no "a" in the string "bcd", it wouldn't find a match.
 
 */
 
-let quoteSample = "3 blind mice.";
-let myRegex = /[^aeiou0-9]/ig; 
-let result = quoteSample.match(myRegex);
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g;
+let result = difficultSpelling.match(myRegex);
 
 
-console.log(result); // (9) [" ", "b", "l", "n", "d", " ", "m", "c", "."]
+console.log(result); // (2) ["ss", "ss"]
