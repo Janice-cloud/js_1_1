ee32282dc909116e0a023cf0c95a40c2d9e8fdb8
Janice Leung <janice.leung2008@gmail.com>
2020-12-14 14:47:51 +1100

    Regular Expressions: Specify Upper and Lower Number of Matches

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Regular Expressions: Match Non-Whitespace Characters
+// Regular Expressions: Specify Upper and Lower Number of Matches
 /* 
 
-Search for non-whitespace using \S, which is an uppercase s. This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. You can think of it being similar to the character class [^ \r\t\f\n\v].
+Plus sign + to look for one or more characters
+The asterisk * to look for zero or more characters.
+These are convenient but sometimes you want to match a certain range of patterns.
 
-let whiteSpace = "Whitespace. Whitespace everywhere!"
-let nonSpaceRegex = /\S/g;
-whiteSpace.match(nonSpaceRegex).length; // Returns 32
+You can specify the lower and upper number of patterns with quantity specifiers. Quantity specifiers are used with curly brackets ({ and }). You put two numbers between the curly brackets - for the lower and upper number of patterns.
+
+To match only the letter a appearing between 3 and 5 times in the string "ah", your regex would be /a{3,5}h/.
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4); // Returns true
+multipleA.test(A2); // Returns false
 
 */
 
-let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /\S/g;
-let result = sample.match(countNonWhiteSpace);
+// match the entire phrase "Oh no" only when it has 3 to 6 letter h's
 
-console.log(result); // (38) ["W", "h", "i", "t", "e", "s", "p", "a", "c", "e", "i", "s", "i", "m", "p", "o", "r", "t", "a", "n", "t", "i", "n", "s", "e", "p", "a", "r", "a", "t", "i", "n", "g", "w", "o", "r", "d", "s"]
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/; 
+let result = ohRegex.test(ohStr); 
+
+console.log(result); // true
