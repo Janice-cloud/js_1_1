bc7ee71389637ea7e0a79a02205bd63b292c447c
Janice Leung <janice.leung2008@gmail.com>
2020-12-16 16:07:50 +1100

    Regular Expressions: Reuse Patterns Using Capture Groups

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-// Regular Expressions: Check For Mixed Grouping of Characters
+// Regular Expressions: Reuse Patterns Using Capture Groups
 /* 
 
-Sometimes we want to check for groups of characters using a Regular Expression and to achieve that we use parentheses ().
+Some patterns you search for will occur multiple times in a string. It is wasteful to manually repeat that regex. There is a better way to specify when you have multiple repeat substrings in your string.
 
-If you want to find either Penguin or Pumpkin in a string, you can use the following Regular Expression: /P(engu|umpk)in/g
+You can search for repeat substrings using capture groups. Parentheses, ( and ), are used to find repeat substrings. You put the regex of the pattern that will repeat in between the parentheses.
 
-Then check whether the desired string groups are in the test string by using the test() method.
+To specify where that repeat string will appear, you use a backslash (\) and then a number. This number starts at 1 and increases with each additional capture group you use. An example would be \1 to match the first group.
 
-let testStr = "Pumpkin";
-let testRegex = /P(engu|umpk)in/;
-testRegex.test(testStr);
-// Returns true
+The example below matches any word that occurs twice separated by a space:
+
+let repeatStr = "regex regex";
+let repeatRegex = /(\w+)\s\1/;
+repeatRegex.test(repeatStr); // Returns true
+repeatStr.match(repeatRegex); // Returns ["regex regex", "regex"]
+Using the .match() method on a string will return an array with the string it matches, along with its capture group.
+
+
 
 */
 
 //  regex checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
 // regex checked against myString and either true or false is returned depending on whether the regex matches.
 
-let myString = "Eleanor Roosevelt";
-let myRegex = /(Franklin|Eleanor).*Roosevelt/; 
-let result = myRegex.test(myString); 
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; 
+let result = reRegex.test(repeatNum);
 
-console.log(result); 
+console.log(result); // TRUE
 
-// .* to allow for middle names.
+// ^ at the beginning of a string
+// \d global search for digits
+// + global search for at least one digit 
+// \s global search for whitespace characters in a string
+// $ global searchfor digit  end of a string
 
