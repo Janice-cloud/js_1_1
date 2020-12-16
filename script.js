1211c830cdb4c7a46564d01d716fd9dc867b4dec
Janice Leung <janice.leung2008@gmail.com>
2020-12-16 15:52:00 +1100

    Regular Expressions: Check For Mixed Grouping of Characters

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-// Regular Expressions: Positive and Negative Lookahead
+// Regular Expressions: Check For Mixed Grouping of Characters
 /* 
 
-Lookaheads are patterns that tell JavaScript to look-ahead in your string to check for patterns further along. This can be useful when you want to search for multiple patterns over the same string.
+Sometimes we want to check for groups of characters using a Regular Expression and to achieve that we use parentheses ().
 
-There are two kinds of lookaheads: positive lookahead and negative lookahead.
+If you want to find either Penguin or Pumpkin in a string, you can use the following Regular Expression: /P(engu|umpk)in/g
 
-A positive lookahead will look to make sure the element in the search pattern is there, but won't actually match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched.
+Then check whether the desired string groups are in the test string by using the test() method.
 
-On the other hand, a negative lookahead will look to make sure the element in the search pattern is not there. A negative lookahead is used as (?!...) where the ... is the pattern that you do not want to be there. The rest of the pattern is returned if the negative lookahead part is not present.
-
-EXAMPLES
-
-let quit = "qu";
-let noquit = "qt";
-let quRegex= /q(?=u)/;
-let qRegex = /q(?!u)/;
-quit.match(quRegex); // Returns ["q"]
-noquit.match(qRegex); // Returns ["q"]
-
-A more practical use of lookaheads is to check two or more patterns in one string. Here is a (naively) simple password checker that looks for between 3 and 6 characters and at least one number:
-
-let password = "abc123";
-let checkPass = /(?=\w{3,6})(?=\D*\d)/;
-checkPass.test(password); // Returns true
+let testStr = "Pumpkin";
+let testRegex = /P(engu|umpk)in/;
+testRegex.test(testStr);
+// Returns true
 
 */
 
-// pwRegex to match passwords that are greater than 5 characters long, do not begin with numbers, and have two consecutive digits.
+//  regex checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
+// regex checked against myString and either true or false is returned depending on whether the regex matches.
 
-let sampleWord = "astronaut";
-let pwRegex = /^\D(?=\w{5})(?=\w*\d{2})/; 
-let result = pwRegex.test(sampleWord);
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/; 
+let result = myRegex.test(myString); 
 
-console.log(result); // false
+console.log(result); 
+
+// .* to allow for middle names.
+
