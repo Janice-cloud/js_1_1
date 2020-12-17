2adc780f39a60361b4621ab659d13612849c6f87
Janice Leung <janice.leung2008@gmail.com>
2020-12-17 16:21:10 +1100

    Debugging: Use the JavaScript Console to Check the Value of a Variable

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// Regular Expressions: Remove Whitespace from Start and End
+// Debugging: Use the JavaScript Console to Check the Value of a Variable
 /* 
 
-Sometimes whitespace characters around strings are not wanted but are there. Typical processing of strings is to remove the whitespace at the start and end of it.
+Both Chrome and Firefox have excellent JavaScript consoles, also known as DevTools, for debugging your JavaScript.
+
+You can find Developer tools in your Chrome's menu or Web Console in Firefox's menu. If you're using a different browser, or a mobile phone, we strongly recommend switching to desktop Firefox or Chrome.
+
+The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool. Placing it at strategic points in your code can show you the intermediate values of variables. It's good practice to have an idea of what the output should be before looking at what it is. Having check points to see the status of your calculations throughout your code will help narrow down where the problem is.
+
+Here's an example to print 'Hello world!' to the console:
+
+console.log('Hello world!');
 
 */
 
-// Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+let a = 5;
+let b = 1;
+a++;
 
-// Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.
+let sumAB = a + b;
+console.log(a); //5
 
-
-
-let hello = "   Hello, World!  ";
-let wsRegex = /^\s+|\s+$/g; 
-let result = hello.replace(wsRegex, ""); 
-
-console.log(result); // "Hellow, World!"
-
-// + search for at least one word character
-// \s white space
-// $ matches anything with number at the end of a string
-// ^ matches anything at the begining of the string
-// g global search
-// | global search to find any of the specified alternatives
-
-// .replace replaces any spaces with no space.
 
