d2b25458a934b7b539943249bf45cb715c7adf7b
Janice Leung <janice.leung2008@gmail.com>
2020-12-17 17:27:19 +1100

    Debugging: Catch Misspelled Variable and Function Names

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-// Debugging: Use typeof to Check the Type of a Variable
+// Debugging: Catch Misspelled Variable and Function Names
 /* 
 
-You can use typeof to check the data structure, or type, of a variable. This is useful in debugging when working with multiple data types. If you think you're adding two numbers, but one is actually a string, the results can be unexpected. Type errors can lurk in calculations or function calls. Be careful especially when you're accessing and working with external data in the form of a JavaScript Object Notation (JSON) object.
+The console.log() and typeof methods are the two primary ways to check intermediate values and types of program output. Now it's time to get into the common forms that bugs take. One syntax-level issue that fast typers can commiserate with is the humble spelling error.
 
-Here are some examples using typeof:
-
-console.log(typeof ""); // outputs "string"
-console.log(typeof 0); // outputs "number"
-console.log(typeof []); // outputs "object"
-console.log(typeof {}); // outputs "object"
-JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
-
-JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
+Transposed, missing, or mis-capitalized characters in a variable or function name will have the browser looking for an object that doesn't exist - and complain in the form of a reference error. JavaScript variable and function names are case-sensitive.
 
 */
 
-// console.log() statements to check the typeof each of the two variables seven and three in the code.
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works
 
-let seven = 7;
-let three = "3";
-console.log(seven + three);
-
-console.log(typeof seven);
-console.log(typeof three);
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`); // Net working capital is: 2
 
 
 
