90a793fb419a23a77c013502c1befec71b4eb401
Janice Leung <janice.leung2008@gmail.com>
2021-01-09 13:39:07 +1100

    Basic Data Structures: Use an Array to Store a Collection of Data

--- script.js
+++ script.js
@@ -35,4 +35,4 @@ let complexArray = [
 
 // We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
 
-let yourArray = ["string", 1, "three", true, false];
+let yourArray = ["string", 1, "three", true, false]; //
