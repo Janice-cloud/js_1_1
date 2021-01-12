f146a379f62f3c6c6bf8415fc20886d7d65fe897
Janice Leung <janice.leung2008@gmail.com>
2021-01-13 10:05:27 +1100

    Basic Data Structures: Combine Arrays with the Spread Operator

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-// Basic Data Structures: Copy an Array with the Spread Operator
+// Basic Data Structures: Combine Arrays with the Spread Operator
 /* 
 
-While slice() allows us to be selective about what elements of an array to copy, among several other useful tasks, ES6's new spread operator allows us to easily copy all of an array's elements, in order, with a simple and highly readable syntax. The spread syntax simply looks like this: ...
+Another huge advantage of the spread operator, is the ability to combine arrays, or to insert all the elements of one array into another, at any index. With more traditional syntaxes, we can concatenate arrays, but this only allows us to combine arrays at the end of one, and at the start of another. Spread syntax makes the following operation extremely simple:
 
-In practice, we can use the spread operator to copy an array like so:
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
 
-let thisArray = [true, true, undefined, false, null];
-let thatArray = [...thisArray];
-// thatArray equals [true, true, undefined, false, null]
-// thisArray remains unchanged and thatArray contains the same elements as thisArray
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+// thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+Using spread syntax, we have just achieved an operation that would have been more complex and more verbose had we used traditional methods.
 
 */
 
-// We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!).
+// We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
 
-function copyMachine(arr, num) {
-  let newArr = [];
-  while (num >= 1) {
-    newArr.push([...arr]);
-
-    num--;
-  }
-  return newArr;
+function spreadOut() {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
 }
 
-console.log(copyMachine([true, false, true], 2));
-
-//The spread operator copies all elements into a new empty object.
-//The code above will copy all of the elements into newArr but will also reinitialise newArr with every new iteration of the while loop.
-//A new variable should first be initialised using the spread operator - let obj = [...arr]; - then this variable should be added to the newArr for every iteration of the while loop.
