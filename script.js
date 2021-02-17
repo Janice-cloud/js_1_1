fdea9c6348dfa6bf14bc19a8958ef200612adff6
Janice Leung <janice.leung2008@gmail.com>
2021-02-17 14:07:31 +1100

    Basic Algorithm Scripting: Factorialize a Number

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Basic Algorithm Scripting: Reverse a String
+// Basic Algorithm Scripting: Factorialize a Number
 
 /* 
+Return the factorial of the provided integer.
 
-The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
 
-You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function.
 
 */
 
-function reverseString(str) {
-    return str.split("").reverse().join("");
+function factorialize(num) {
+  for (var product = 1; num > 0; num--) {
+    product *= num;
   }
-  
-  console.log(reverseString("hello"));
+  return product;
+}
+
+console.log(factorialize(5));
+
+/*
+Code Explanation
+Since the return values for the function will always be greater than or equal to 1, product is initialized at one. For the case where the number is 0, the for loop condition will be false, but since product is initialized as 1, it will have the correct value when the return statement is executed.
+
+For all numbers passed to the function which are greater than one, the simple for loop will decrement num by one each iteration and recalculate product down to the value 1.
+*/
