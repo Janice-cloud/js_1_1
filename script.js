e91c6391f1959560ce1677cc6e9aadf35c46c3df
Janice Leung <janice.leung2008@gmail.com>
2021-03-22 10:32:23 +1100

    Basic Algorithm Scripting: Repeat a String Repeat a String

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// Basic Algorithm Scripting: Confirm the Ending
+// Basic Algorithm Scripting: Repeat a String Repeat a String
 
 /* 
-Check if a string (first argument, str) ends with the given target string (second argument, target).
-
-This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 
 */
 
-function confirmEnding(str, target) {
+function repeatStringNumTimes(str, num) {
+  var accumulatedStr = "";
 
-  return str.slice(str.length - target.length) === target;
-}
+  while (num > 0) {
+    accumulatedStr += str;
+    num--;
+  }
 
-console.log(confirmEnding("He has to give me a new name", "name"));
+  return accumulatedStr;
+};
+
+
+console.log(repeatStringNumTimes("abc", 4));
 
 /*
 Code Explanation
-First we use the slice method copy the string.
-In order to get the last characters in str equivalent to the target's length we use the slice method.
-The first parameter inside the slice method is the starting index and the second parameter would be the ending index.
-For example str.slice(10, 17) would return give me.
-In this case we only include one parameter which it will copy everything from the starting index.
-We substract the length of str and the length of target, that way, we shall get the last remaining characters equivalent to the target's length.
-Finally we compare the return result of slice to target and check if they have the same characters.
+Create an empty string variable to store the repeated word.
+Use a while loop or for loop to repeat code as many times as needed according to num
+Then we just have to add the string to the variable created on step one, and increase or decrease num depending on how you set the loop.
+At the end of the loop, return the variable for the repeated word.
+
 */
